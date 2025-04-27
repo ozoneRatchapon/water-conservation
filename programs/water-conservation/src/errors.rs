b0410d65be502115674ac839c9ade4f24163ed24
ozoneRatchapon <61614162+ozoneRatchapon@users.noreply.github.com>
2025-04-27 17:41:08 +0700

    Rename WaterConservationError to GreenmoveError with new codes

--- programs/water-conservation/src/errors.rs
+++ programs/water-conservation/src/errors.rs
@@ -1,11 +1,21 @@
 use anchor_lang::error_code;
 
 #[error_code]
-pub enum WaterConservationError {
+pub enum GreenmoveError {
     InvalidAmount,
     InsufficientBalance,
     InvalidState,
     InvalidInstruction,
     #[msg("Invalid Depin Feed Address")]
     InvalidDepinFeedAddress,
+    #[msg("Invalid Property Account")]
+    InvalidPropertyAccount,
+    #[msg("Invalid Timestamp")]
+    InvalidTimestamp,
+    #[msg("Timestamps are out of order")]
+    TimestampsOutOfOrder,
+    #[msg("Excessive energy consumption")]
+    ExcessiveEnergyConsumption,
+    #[msg("Excessive water consumption")]
+    ExcessiveWaterConsumption,
 }
